e97716594438ca60d787fdfe6e6a6444bac7537b
jquense <monastic.panic@gmail.com>
2014-11-09 11:54:48 -0500

    sa

--- dev/dev.jsx
+++ dev/dev.jsx
@@ -5,34 +5,58 @@ require('../src/less/bbm-complete.less')
 var React = require('react/addons')
   , Modal = require('../src/components/Modal.jsx')
   , ModalTrigger = require('../src/components/ModalTrigger')
+  , Layer = require('../src/Layer')
 
+
+function alert(message, callback) {
+
+  var layer = new Layer(document.body, function(){
+
+    return (
+      <Modal show={true} onRequestHide={finish} backdrop='static'>
+        <div className="modal-body">
+            <h4>{message}</h4>
+        </div>
+        <div className="modal-footer">
+            <button onClick={finish}>Close</button>
+        </div>
+      </Modal>)
+
+    function finish(){
+      callback()
+      setTimeout(function(){ 
+        layer.destroy() 
+      })
+    }
+  })
+
+  layer.render()
+}
+
+
+function renderNested() {
+  return (
+      <Modal {...this.props} title="Modal heading">
+        <div className="modal-body">
+          <h4>Text in a modal</h4>
+
+          <h4>Overflowing text to show scroll behavior</h4>
+          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+
+          <ModalTrigger modal={<MyModal />}>
+            <button type='button'>Launch Modal-ception</button>
+          </ModalTrigger>
+        </div>
+        <div className="modal-footer">
+          <button onClick={this.props.onRequestHide}>Close</button>
+        </div>
+      </Modal>
+    );
+}
 
 var NestedModal = React.createClass({
-
-  render: function() {
-    return (
-        <Modal {...this.props} title="Modal heading">
-          <div className="modal-body">
-            <h4>Text in a modal</h4>
-
-            <h4>Overflowing text to show scroll behavior</h4>
-            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-
-            <ModalTrigger modal={<MyModal />}>
-              <button type='button'>Launch Modal-ception</button>
-            </ModalTrigger>
-          </div>
-          <div className="modal-footer">
-            <button onClick={this.props.onRequestHide}>Close</button>
-          </div>
-        </Modal>
-      );
-  }
+  render: renderNested
 });
-
-function h(p){
-  return function(){console.log(p)}
-}
 
 var MyModal = React.createClass({
 
